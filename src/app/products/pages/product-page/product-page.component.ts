d395b6c850858c98a83d794f1f0de6fb9c208e4b
ivanroscuenca <ivanros@protonmail.com>
2023-09-20 17:51:50 +0200

    video313

--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.css']
+    templateUrl: "./product-page.component.html",
+    styleUrls: ["./product-page.component.css"],
 })
 export class ProductPageComponent {
+    //crear formulario reactivo : 2 maneras
+    //antigua inyección con constructor
+    //constructor(private fb:FormBuilder){}
 
+    //nueva forma con inject
+    private fb = inject(FormBuilder);
+
+    public color: string = "green";
+    public myForm: FormGroup = this.fb.group({
+        name: [
+            "",
+            [Validators.required, Validators.minLength(6), Validators.email],
+        ],
+    });
+
+    changeColor() {
+        const color = "#xxxxxx".replace(/x/g, (y) =>
+            ((Math.random() * 16) | 0).toString(16)
+        );
+        this.color = color;
+    }
 }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { ProductsRoutingModule } from "./products-routing.module";
-import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { ProductPageComponent } from "./pages/product-page/product-page.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-    declarations: [
-    ProductPageComponent
-  ],
-    imports: [CommonModule, ProductsRoutingModule],
+    declarations: [ProductPageComponent],
+    imports: [CommonModule, ProductsRoutingModule, ReactiveFormsModule],
 })
 export class ProductsModule {}
