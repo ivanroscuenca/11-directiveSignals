70429034406996b2e84331306933c2cf0a01421c
ivanros <ivanroscuenca@gmail.com>
2023-09-26 15:06:22 +0200

    video324

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from "@angular/core";
 
 @Component({
-  templateUrl: './counter-page.component.html',
-  styleUrls: ['./counter-page.component.css']
+    templateUrl: "./counter-page.component.html",
+    styleUrls: ["./counter-page.component.css"],
 })
 export class CounterPageComponent {
-
+    public counter = signal(10);
+    //signal computada, solo lectura
+    public squareCounted = computed(() => this.counter() * this.counter());
+    decrease() {
+        this.counter.set(this.counter() - 1);
+    }
+    increase() {
+        this.counter.set(this.counter() + 1);
+    }
 }
