5cb09bc694ce3626e1557b0eb3003dd1f7fedb86
ivanros <ivanroscuenca@gmail.com>
2023-09-26 17:42:07 +0200

    video327

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, signal } from "@angular/core";
+import { Component, OnInit, computed, inject, signal } from "@angular/core";
 import { UsersServiceService } from "../../services/users-service.service";
 import { User } from "../../interfaces/user-request.interface";
 
@@ -8,11 +8,19 @@ import { User } from "../../interfaces/user-request.interface";
 })
 export class UserInfoPageComponent implements OnInit {
     //inyectamos servicio
+
     private usersService = inject(UsersServiceService);
     public userId = signal(1);
 
     public currentUser = signal<User | undefined>(undefined);
     public userWasFound = signal(true);
+
+    public fullName = computed<string>(() => {
+        if (!this.currentUser()) return "Usuario no encontrado";
+        return `${this.currentUser()?.first_name} ${
+            this.currentUser()?.last_name
+        }`;
+    });
 
     ngOnInit(): void {
         this.loadUser(this.userId());
@@ -20,10 +28,19 @@ export class UserInfoPageComponent implements OnInit {
 
     loadUser(id: number) {
         if (id <= 0) return;
-        this.userId.set(id);
 
-        this.usersService.getUserById(id).subscribe((user) => {
-            this.currentUser.set(user);
+        this.userId.set(id);
+        this.currentUser.set(undefined);
+
+        this.usersService.getUserById(id).subscribe({
+            next: (user) => {
+                this.currentUser.set(user);
+                this.userWasFound.set(true);
+            },
+            error: () => {
+                this.userWasFound.set(false);
+                this.currentUser.set(undefined);
+            },
         });
     }
 }
