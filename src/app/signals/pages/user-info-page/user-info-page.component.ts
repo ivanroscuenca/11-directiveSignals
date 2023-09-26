8d512d1215f4ac7ee04975dc0f1ed612c9ad8d9b
ivanros <ivanroscuenca@gmail.com>
2023-09-26 17:20:56 +0200

    video326

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from "@angular/core";
+import { UsersServiceService } from "../../services/users-service.service";
+import { User } from "../../interfaces/user-request.interface";
 
 @Component({
-  templateUrl: './user-info-page.component.html',
-  styleUrls: ['./user-info-page.component.css']
+    templateUrl: "./user-info-page.component.html",
+    styleUrls: ["./user-info-page.component.css"],
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+    //inyectamos servicio
+    private usersService = inject(UsersServiceService);
+    public userId = signal(1);
 
+    public currentUser = signal<User | undefined>(undefined);
+    public userWasFound = signal(true);
+
+    ngOnInit(): void {
+        this.loadUser(this.userId());
+    }
+
+    loadUser(id: number) {
+        if (id <= 0) return;
+        this.userId.set(id);
+
+        this.usersService.getUserById(id).subscribe((user) => {
+            this.currentUser.set(user);
+        });
+    }
 }
